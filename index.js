70165c6688497dc4fdd1de15d2c3eedfaba0dcf5
Patrick Wozniak <patrick.wozniak@cegeka.de>
2019-08-27 08:57:51 +0200

    feat(nav): use pathname to display different components

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Timezones from './src/Timezones';
 
+function App({ apiUrl }) {
+  const path = window.location.pathname;
+  return (
+    <>
+      <header>
+        <h1>React Basics Cegeka Workshop</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/timezones">Timezones</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <hr />
+      <main>
+        {path === '/' && 'Welcome Cegeka!'}
+        {path === '/timezones' && <Timezones apiUrl={apiUrl} />}
+      </main>
+    </>
+  );
+}
+
 var mountNode = document.getElementById('app');
-ReactDOM.render(<Timezones apiUrl={process.env.API_URL || '/api'} />, mountNode);
+ReactDOM.render(<App apiUrl={process.env.API_URL || '/api'} />, mountNode);
